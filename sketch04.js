155b6584208cb7279c5cd3f7fc425e5bbf0e156e
V Wong <veritywong90@gmail.com>
2023-08-22 16:05:21 +0100

    moving dots/agents

--- sketch04.js
+++ sketch04.js
@@ -2,7 +2,8 @@ const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random')
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true
 };
 
 const sketch = ({ context, width, height }) => {
@@ -19,15 +20,17 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    agents.forEach( agent => {
+    agents.forEach(agent => {
+      agent.update();
       agent.draw(context);
+      agent.bounce(width, height)
     })
   };
 };
 
 canvasSketch(sketch, settings);
 
-class Point {
+class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
@@ -36,14 +39,32 @@ class Point {
 
 class Agent {
   constructor(x, y) {
-    this.pos = new Point(x, y);
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
     this.radius = 10;
   }
   
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
+
   draw(context) {
-    context.fillStyle = 'black';
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+
+    context.lineWidth = 4;
+
     context.beginPath();
-    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2)
+    context.arc(0, 0, this.radius, 0, Math.PI * 2)
     context.fill();
+    context.stroke();
+
+    context.restore();
+  }
+
+  bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1;
+    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1;
   }
 }
